81da9265b6139602487cd62a1e03fe43aad107dc
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-03-19 10:16:21 -0600

    Define multiple themes.

--- src/example/slider-overview-example.ts
+++ src/example/slider-overview-example.ts
@@ -13,12 +13,21 @@ import {MatSliderModule} from '@angular/material/slider';
   imports: [MatSliderModule, CommonModule],
 })
 export class SliderOverviewExample {
-  useOtherTheme: boolean = true;
   useOtherColor: boolean = true;
 
+  private themeNames: string[] = ['theme-1', 'theme-2', 'theme-3'];
+  private themeIndex: number = 0;
+
+  useOtherTheme: string = this.themeNames[this.themeIndex];
+
+  theme() {
+    this.themeIndex++;
+
+    if (this.themeIndex == this.themeNames.length) {
+      this.themeIndex = 0;
+    }
+
+    return this.themeNames[this.themeIndex];
+  }
+
 }
-
-
-/**  Copyright 2024 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
